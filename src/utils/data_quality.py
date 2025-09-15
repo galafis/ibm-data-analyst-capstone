e73058f7702974a05ff1639ea122393f584182bb
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2025-09-15 13:52:33 -0300

    Create data_quality.py

---
+++ src/utils/data_quality.py
@@ -0,0 +1,27 @@
+"""
+Módulo: data_quality.py
+Função: Fornece utilidades para validação de datasets, integridade e consistência dos dados em projetos de Data Science.
+
+Exemplo de uso planejado:
+
+from src.utils.data_quality import DataQualityChecker
+
+checker = DataQualityChecker()
+quality_report = checker.validate_dataset(data, rules=['no_nulls', 'valid_dates', 'positive_amounts'])
+print(quality_report)
+
+Desenvolva as funções e classes conforme a necessidade do seu projeto.
+"""
+
+class DataQualityChecker:
+    def __init__(self):
+        pass
+
+    def validate_dataset(self, data, rules=None):
+        """
+        Realiza validações básicas no dataset.
+        Parâmetros:
+            data: dataset (ex: DataFrame)
+            rules: lista de regras (strings) a serem aplicadas
+        """
+        return "Função de validação ainda não implementada."
